714482fadab3dd1d75bdbabd0799f0da09b9ab9b
rick <riky.lastrico@gmail.com>
2017-03-03 12:53:43 +0100

    v1

---
+++ hello world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hellooo c# world");
+            printlogo();
+        }
+        static void printlogo()
+        {
+            System.Console.WriteLine("made by rk");
+        }
+    }
+}
